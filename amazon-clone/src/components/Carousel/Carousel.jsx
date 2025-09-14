32cf643401c67ff27e0feeb939a21ee6f23281a4
bethelehem12 <verolina955@gmail.com>
2025-09-14 15:29:28 -0400

    Add Header

---
+++ amazon-clone/src/components/Carousel/Carousel.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+const Carousel = () => {
+  return (
+    <div>
+      <Carousel>autoPlay={true}</Carousel>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ amazon-clone/src/components/Header/Header.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import US from "../../assets/img/US.png";
+import classes from "./Header.module.css";
+import { SlLocationPin } from "react-icons/sl";
+import { BsSearch } from "react-icons/bs";
+import { BiCart } from "react-icons/bi";
+import LowerHeader from "./LowerHeader";
+const Header = () => {
+  return (
+    <>
+      <section>
+        <div className={classes.header_container}>
+          {/* logo container*/}
+          <div className={classes.logo_container}>
+            <a href="/">
+              <img
+                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon header logo"
+              />
+            </a>
+            {/* delivery */}
+            <div className={classes.delivery}>
+              <span>
+                <SlLocationPin />
+              </span>
+              <div>
+                <p>Deliver to</p>
+                <span>USA</span>
+              </div>
+            </div>
+          </div>
+          {/* search section */}
+          <div className={classes.search}>
+            <select name="" id="">
+              <option value="">All</option>
+            </select>
+            <input type="text" name="" id="" placeholder="search product" />
+            <BsSearch size={25} />
+          </div>
+
+          {/* right side link */}
+          <div className={classes.order_container}>
+            <a href="" className={classes.language}>
+              <img src={US} alt="USA flag" />
+              <select>
+                <option value="EN"></option>
+              </select>
+            </a>
+
+            {/* three components */}
+
+            <a href="">
+              <p>Sign In</p>
+              <span>Account & Lists</span>
+            </a>
+            {/* orders */}
+            <a href="">
+              <p>returns</p>
+              <span>& Orders</span>
+            </a>
+            {/* cart */}
+            <a href="" className={classes.cart}>
+              <BiCart size={35} />
+              <span>0</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <LowerHeader/>
+    </>
+  );
+};
+
+export default Header;
